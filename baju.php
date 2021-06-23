39cf33c43c81ff69c9bf4c25b97c98687200f7c1
mustaqim2you <86315565+mustaqim2you@users.noreply.github.com>
2021-06-23 22:55:06 +0800

    Add files via upload

---
+++ baju.php
@@ -0,0 +1,25 @@
+<body>
+    <form class="row g-3">
+        <div class="col-md-6">
+            <label for="inputNama" class="form-label">Nama</label>
+            <input type="text" class="form-control" id="inputNama">
+        </div>
+        <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Email</label>
+            <input type="email" class="form-control" id="inputEmail4">
+        </div>
+        <div class="col-md-4">
+            <label for="inputSize" class="form-label">Saiz</label>
+            <select id="inputSize" class="form-select">
+                <option selected>Pilihan Saiz</option>
+                <option>Saiz S</option>
+                <option>Saiz M</option>
+                <option>Saiz L</option>
+                <option>Saiz XL</option>
+            </select>
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Tempah</button>
+        </div>
+    </form>
+</body>    
